f29771f550746defb8441864ee746b0006001120
Mehedi Hasan <mehedihasanhrid1@gmail.com>
2025-07-24 14:16:21 +0600

    lesson 2

---
+++ activity_1_simple_calculator.py
@@ -0,0 +1,16 @@
+# Take two numbers as input
+a = int(input("Enter first number (a): "))
+b = int(input("Enter second number (b): "))
+
+# Addition
+print("Addition:", a + b)  # Adds a and b
+
+# Subtraction
+print("Subtraction:", a - b)  # Subtracts b from a
+
+# Multiplication
+print("Multiplication:", a * b)  # Multiplies a and b
+
+# Division
+print("Division:", a / b)  # Divides a by b
+
