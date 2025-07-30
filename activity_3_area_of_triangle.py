dbd2183d92b1c3eda98655425024c3f0c41a25b1
Mehedi Hasan <mehedihasanhrid1@gmail.com>
2025-07-30 14:59:13 +0600

    area of triangle

---
+++ activity_3_area_of_triangle.py
@@ -0,0 +1,9 @@
+# Input base and height from user
+base = float(input("Enter the base of the triangle: "))
+height = float(input("Enter the height of the triangle: "))
+
+# Calculate area
+area = 0.5 * base * height
+
+# Display the result
+print(f"The area of the triangle is {area}")
